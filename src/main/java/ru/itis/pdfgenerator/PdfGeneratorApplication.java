45f52cc945d97fb789fd974dfc3d64e73ff9553a
azaliyas-github <1maxovmax1@gmail.com>
2022-01-22 16:25:25 +0300

    Initialize project

---
+++ src/main/java/ru/itis/pdfgenerator/PdfGeneratorApplication.java
@@ -0,0 +1,11 @@
+package ru.itis.pdfgenerator;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class PdfGeneratorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PdfGeneratorApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/itis/pdfgenerator/PdfGeneratorApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.itis.pdfgenerator;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class PdfGeneratorApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
